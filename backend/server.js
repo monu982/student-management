bb168caa28003f8b57b0ff5c2150945cd76503ab
Manish Singh <manisharmy004@gmail.com>
2025-08-03 12:03:46 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const studentRoutes = require("./routes/studentRoutes");
+const dotenv = require("dotenv").config();
 
 dotenv.config();
 const app = express();
@@ -22,7 +23,7 @@ app.get("/", (req, res) => {
 app.use("/api/students", studentRoutes);
 
 // Start Server
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
