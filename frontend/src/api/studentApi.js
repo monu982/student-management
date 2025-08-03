20efe018cfc106f6fd28b7b062ab1afc5ca9f9a1
Manish Singh <manisharmy004@gmail.com>
2025-08-03 12:38:43 +0530

    init

--- frontend/src/api/studentApi.js
+++ frontend/src/api/studentApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/students";
+const API_URL = import.meta.env.VITE_BACKEND_URL;
 
 // Fetch all students
 export const fetchStudents = async () => {
