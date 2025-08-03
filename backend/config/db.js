0101c973f7a1570af5dd824984c04e5152ff851a
Manish Singh <manisharmy004@gmail.com>
2025-08-03 11:43:36 +0530

    add env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 const path = require("path");
+const dotenv = require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://monu982:tNsYXHmnTZ2QwJ5u@student-management.d0kosv3.mongodb.net/?retryWrites=true&w=majority&appName=Student-management"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connection successful");
   } catch (error) {
     console.log("MongoDB Connection Failed", error);
